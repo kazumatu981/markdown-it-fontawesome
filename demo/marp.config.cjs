98428102d393d40284f89672affc5cf77f166160
kazumatu981 <kazumatu981@gmail.com>
2023-04-07 07:58:12 +0900

    remove redundant configure of  `demo`.

--- demo/marp.config.cjs
+++ demo/marp.config.cjs
@@ -4,11 +4,4 @@ module.exports = {
     themeSet: './my-theme.css',
     engine: ({ marp }) => marp
         .use(require('../index'))
-    // .use(require('markdown-it-attrs'))
-    // .use(require('markdown-it-bracketed-spans'))
-    ,
-
-    html: {
-        i: ["class"],
-    }
 }
