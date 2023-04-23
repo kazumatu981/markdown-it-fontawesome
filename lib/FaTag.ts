633be996e66dc8f2eca815cd8df79774639b294c
kazumatu981 <kazumatu981@gmail.com>
2023-04-23 12:24:18 +0900

    FaTag Complete

--- lib/FaTag.ts
+++ lib/FaTag.ts
@@ -9,12 +9,12 @@ import {
 
 abstract class FaTagBase {
     public kind: FaTagKind
-    constructor(detectedFaTag: DetectedFaTag) {
-        this.kind = detectedFaTag.kind;
-        this._parse(detectedFaTag);
+    constructor(parsed: DetectedSimpleTag | DetectedStackingTag, _kind: FaTagKind) {
+        this.kind = _kind;
+        this._setParsed(parsed);
     }
 
-    protected abstract _parse(detectFaTag: DetectedFaTag): void;
+    protected abstract _setParsed(parsed: DetectedSimpleTag | DetectedStackingTag): void;
 
     detectFaTag(str: string, pos: number, ignoreStyled: boolean): FaTagBase | null {
         // if not start with 0x3A(:) or 0x5B([) then returns null.
@@ -26,12 +26,12 @@ abstract class FaTagBase {
         // detect!
         let detected = _detectSimpleTag(source, ignoreStyled);
         if (detected !== null) {
-            return new SimpleFaTag(detected)
+            return new SimpleFaTag(detected.parsed)
         }
         else {
             detected = _detectStackingTag(source, ignoreStyled);
             if (detected !== null) {
-                return new StackingFaTag(detected);
+                return new StackingFaTag(detected.parsed);
             }
         }
         return null;
@@ -41,12 +41,32 @@ abstract class FaTagBase {
 
 class SimpleFaTag extends FaTagBase {
     icons: string[] = [];
-    styles?: string[];
+    styles: string[] = [];
+    constructor(parsed: DetectedSimpleTag | DetectedStackingTag) {
+        super(parsed, "simple");
+    }
+    protected _setParsed(parsed: DetectedSimpleTag | DetectedStackingTag): void {
+        const detectedSimpleTag = <DetectedSimpleTag>parsed;
+        this.icons.push(...detectedSimpleTag.faClasses
+            .split(' ')
+            .filter((item) => item !== ''));
+        if (detectedSimpleTag.styleClasses !== null) {
+            this.styles.push(...detectedSimpleTag.styleClasses
+                .split(' ')
+                .filter(item => item != '')
+                .map(styleCalss => styleCalss.replace('.', '')));
+        }
+    }
+
 }
 
 class StackingFaTag extends FaTagBase {
     children: SimpleFaTag[] = [];
-    protected _parse(detectFaTag: DetectedFaTag): void {
-        const detectedStackingFaTag = <DetectedStackingTag>detectFaTag;
+    constructor(parsed: DetectedSimpleTag | DetectedStackingTag) {
+        super(parsed, "stacking");
+    }
+    protected _setParsed(parsed: DetectedSimpleTag | DetectedStackingTag): void {
+        const detectedStackingFaTag = <DetectedStackingTag>parsed;
+        this.children.push(...detectedStackingFaTag.map(item => new SimpleFaTag(item)))
     }
 }
