c37b98dc5ccd3e0dd0a1e0c216a75d6c161461e9
Kazuyoshi Matsumoto <kazumatu981@gmail.com>
2025-07-16 20:36:24 +0900

    feat: enhance TagDetector options and improve documentation

--- lib/TagDetector.ts
+++ lib/TagDetector.ts
@@ -10,11 +10,19 @@ const DEFAULT_SIMPLE_FA_TAG_END = ':';
 const DEFAULT_STACKING_FA_TAG_START = '[';
 const DEFAULT_STACKING_FA_TAG_END = ']';
 
+/**
+ * Options for TagDetector
+ */
 export interface TagDetectorOptions {
+    /** ignore styled fa tags */
     ignoreStyled?: boolean;
+    /** start tag for simple fa tags */
     simpleFaTagStart?: string;
+    /** end tag for simple fa tags */
     simpleFaTagEnd?: string;
+    /** start tag for stacking fa tags */
     stackingFaTagStart?: string;
+    /** end tag for stacking fa tags */
     stackingFaTagEnd?: string;
 }
 
@@ -30,6 +38,9 @@ export interface DetectedTag {
     parsed: ParsedTag;
 }
 
+/**
+ * The parsed tag object.
+ */
 export type ParsedTag = ParsedFaTag | ParsedStackingTag;
 
 /**
@@ -37,51 +48,89 @@ export type ParsedTag = ParsedFaTag | ParsedStackingTag;
  */
 export type FaTagKind = 'fa' | 'stacking-fa';
 
+/**
+ * Parsed Fontawesome tag
+ */
 export interface ParsedFaTag {
+    /** Fontawesome classes */
     faClasses: string;
+    /** Style classes, if any */
     styleClasses: string | null;
 }
 
+/**
+ * Parsed stacking Fontawesome tag
+ */
 export type ParsedStackingTag = ParsedFaTag[];
 
+/**
+ * TagDetector:
+ * Detects Fontawesome tags in a given source string.
+ * It can detect both simple and stacking Fontawesome tags.
+ */
 export class TagDetector {
+    // #region "private fields"
     private readonly _options?: TagDetectorOptions;
-    constructor(options?: TagDetectorOptions) {
+    // #endregion
+
+    // #region "constructor"
+    /**
+     * Creates an instance of TagDetector.
+     * @param options - Options for the tag detector.
+     */
+    public constructor(options?: TagDetectorOptions) {
         this._options = options;
     }
+    // #endregion
 
+    // #region "public properties"
+    /**
+     * Gets the start tag for simple Fontawesome tags.
+     * @returns The start tag for simple Fontawesome tags.
+     */
     public get simpleFaTagStart(): string {
         return this._options?.simpleFaTagStart ?? DEFAULT_SIMPLE_FA_TAG_START;
     }
+    /**
+     * Gets the end tag for simple Fontawesome tags.
+     * @returns The end tag for simple Fontawesome tags.
+     */
     public get simpleFaTagEnd(): string {
         return this._options?.simpleFaTagEnd ?? DEFAULT_SIMPLE_FA_TAG_END;
     }
+    /**
+     * Gets the start tag for stacking Fontawesome tags.
+     * @returns The start tag for stacking Fontawesome tags.
+     */
     public get stackingFaTagStart(): string {
         return this._options?.stackingFaTagStart ?? DEFAULT_STACKING_FA_TAG_START;
     }
+    /**
+     * Gets the end tag for stacking Fontawesome tags.
+     * @returns The end tag for stacking Fontawesome tags.
+     */
     public get stackingFaTagEnd(): string {
         return this._options?.stackingFaTagEnd ?? DEFAULT_STACKING_FA_TAG_END;
     }
+    // #endregion
 
-    public mustBeAFaTag(str: string) {
+    // #region "public methods"
+    /**
+     * Checks if the given string must be a Fontawesome tag.
+     * @param str - The string to check.
+     * @returns True if the string must be a Fontawesome tag, otherwise false.
+     */
+    public mustBeAFaTag(str: string): boolean {
         return str.startsWith(this.simpleFaTagStart) || str.startsWith(this.stackingFaTagStart);
     }
 
-    private get simpleFaTagPattern() {
-        return `\\${this.simpleFaTagStart}${FA_CLASSES_TAG_PATTERN}\\${this.simpleFaTagEnd}`;
-    }
-    private get styledFaTagPattern() {
-        return `\\[ *${this.simpleFaTagPattern} *\\]\\{ *${STYLE_CLASSES_TAG_PATTERN} *\\}`;
-    }
-    protected get faTagPattern() {
-        return `((${this.simpleFaTagPattern})|(${this.styledFaTagPattern}))`;
-    }
-    protected get stackingFaTagPattern() {
-        return `\\${this.stackingFaTagStart}( *${this.faTagPattern}){2,} *\\${this.stackingFaTagEnd}`;
-    }
-
+    /**
+     * Detects a simple Fontawesome tag in the given source string.
+     * @param source - The source string to search in.
+     * @returns DetectedTag if a simple Fontawesome tag is found, otherwise null.
+     */
     public detectFaTag(source: string): DetectedTag | null {
-        const result = this._detectRegEx(source, this.faTagPattern);
+        const result = this._detectRegEx(source, this._faTagPattern);
         if (result === null) {
             return null;
         }
@@ -97,8 +146,13 @@ export class TagDetector {
             parsed: this._parseFaTag(result),
         };
     }
+    /**
+     * Detects a stacking Fontawesome tag in the given source string.
+     * @param source - The source string to search in.
+     * @returns DetectedTag if a stacking Fontawesome tag is found, otherwise null.
+     */
     public detectStackingTag(source: string): DetectedTag | null {
-        const result = this._detectRegEx(source, this.stackingFaTagPattern);
+        const result = this._detectRegEx(source, this._stackingFaTagPattern);
         if (result === null) {
             return null;
         }
@@ -113,7 +167,25 @@ export class TagDetector {
             parsed: this._parseStackingTag(result),
         };
     }
+    // #endregion
 
+    // #region "non-public members"
+    // #region "private properties"
+    private get _faTagPattern(): string {
+        return `((${this._simpleFaTagPattern})|(${this._styledFaTagPattern}))`;
+    }
+    private get _stackingFaTagPattern(): string {
+        return `\\${this.stackingFaTagStart}( *${this._faTagPattern}){2,} *\\${this.stackingFaTagEnd}`;
+    }
+    private get _simpleFaTagPattern(): string {
+        return `\\${this.simpleFaTagStart}${FA_CLASSES_TAG_PATTERN}\\${this.simpleFaTagEnd}`;
+    }
+    private get _styledFaTagPattern(): string {
+        return `\\[ *${this._simpleFaTagPattern} *\\]\\{ *${STYLE_CLASSES_TAG_PATTERN} *\\}`;
+    }
+    // #endregion
+
+    // #region "private methods"
     private _parseFaTag(tag: string): ParsedFaTag {
         // read fa classes
         const faFound = <RegExpExecArray>new RegExp(FA_CLASSES_TAG_PATTERN, 'g').exec(tag);
@@ -127,7 +199,7 @@ export class TagDetector {
 
     private _parseStackingTag(tag: string): ParsedStackingTag {
         const found: ParsedStackingTag = [];
-        const regex = new RegExp(this.faTagPattern, 'g');
+        const regex = new RegExp(this._faTagPattern, 'g');
         let fatag: RegExpExecArray | null;
         while ((fatag = regex.exec(tag)) !== null) {
             found.push(this._parseFaTag(fatag[0]));
@@ -139,4 +211,6 @@ export class TagDetector {
         const detected = new RegExp(pattern, 'g').exec(source);
         return detected?.index === 0 ? detected[0] : null;
     }
+    // #endregion
+    // #endregion
 }
